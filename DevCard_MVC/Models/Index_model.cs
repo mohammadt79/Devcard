e313fda607635c0bc56ce092df9539ae83918c15
mohammadt2001 <aria.persian.programer@gmail.com>
2022-07-11 12:00:25 +0430

    Project section from index page

---
+++ DevCard_MVC/Models/Index_model.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace DevCard_MVC.Models
+{
+    // public class Index_model
+    // {
+    //     public List<Article> Article { get; set; }
+    //     public List<project> project { get; }
+    // }
+}
